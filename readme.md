50f739029898a36fd8ce9a95aa6a474cbd0c9e89
Munzy <cm@munroenet.com>
2019-12-06 19:57:11 -0800

    Ship it!

---
+++ readme.md
@@ -0,0 +1,24 @@
+# MXRoute Botnet Attempts
+
+This is a quick parsing of the data posted in this tweet: https://twitter.com/MXroute/status/1203057583352958979
+
+## Goals
+
+Append data to the list and make it easily browsable.
+
+Have it run with NodeJS.
+
+## Run it yourself
+
+```
+npm install line-reader
+npm install https
+
+node runner.js
+```
+
+### To File
+
+```
+node runner.js > list.csv
+```
---
+++ runner.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const lineReader = require('line-reader');
+const https = require('https');
+
+function get(URL, callback){
+
+    https.get(URL, function(res){
+
+        // Create our data holder
+        var response = '';
+
+        // Append
+        res.on('data', function(chunk){
+
+            // Append to reponse
+            response += chunk;
+
+        });
+
+        res.on('end', function(){
+
+            try {
+
+                var data = JSON.parse(response);
+                callback(data);
+
+            }
+            catch(e){
+
+                get(URL, callback);                
+
+            }
+            
+
+        });
+
+    
+    })
+
+    // Catch all errors
+    .on('error', function(err){
+
+        get(URL, callback);
+    
+    });
+    
+
+}
+
+
+function asn(ip, callback){
+
+    var URL = 'https://ip2asn.ipinfo.app/lookup/' + ip;
+
+    get(URL, function(data){
+
+        callback(data.announcedBy);
+
+    });
+
+
+}
+
+
+var report = new Array();
+function reader(){
+
+    lineReader.eachLine('botnet.txt', function(ip){
+
+        asn(ip, function(asn){
+
+            asn = asn.reverse();
+
+            if(asn && asn[0] && asn[0].asn){
+
+                console.log('"' + ip + '", "' + asn[0].asn + '", "' + asn[0].name + '", "' + asn[0].subnet + '"');
+
+            }   
+
+        });
+
+
+    });
+
+}
+
+reader();
